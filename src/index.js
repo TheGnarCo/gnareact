b3a774456c53b41cb5160bbffd10e191854ecd8f
Mike Stone <mike@gnar.dog>
2017-04-16 11:37:43 -0400

    Add react

--- src/index.js
+++ src/index.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+  return (
+    <h1>Hello world</h1>
+  );
+};
+
+document.addEventListener('DOMContentLoaded', () => {
+  const mountNode = document.getElementById('app');
+
+  ReactDOM.render(<App />, mountNode);
+});
