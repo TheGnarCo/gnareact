a514bf8bbf870f63bd1c6d1d50043618f827f079
Mike Stone <mike@gnar.dog>
2017-04-16 11:14:02 -0400

    Add webpack

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  context: path.join(__dirname, 'src'),
+  entry: [
+    './index.js',
+  ],
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: [
+          'babel-loader',
+        ],
+      },
+    ],
+  },
+  resolve: {
+    modules: [
+      path.join(__dirname, 'node_modules'),
+      path.join(__dirname, 'src'),
+    ],
+  },
+};
